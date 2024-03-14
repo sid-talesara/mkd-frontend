19f33011518067da95632e114fe5998976c61e24
sid-talesara <2020pcecssiddharth207@poornima.org>
2024-03-14 14:24:46 +0530

    Blog cache update

--- app/(website)/blogs/BlogsPage.tsx
+++ app/(website)/blogs/BlogsPage.tsx
@@ -1,13 +1,38 @@
+// import React from 'react';
+// import BlogsHero from './BlogsHero';
+
+// import { getAllPublished } from '../../lib/notion';
+// import BlogsCards from './BlogsCards';
+// import { BlogCardData } from '@/shared/types';
+// export const revalidate = 10;
+// const BlogsPage = async () => {
+//   const posts = await getAllPublished();
+//   console.log(posts);
+//   return (
+//     <>
+//       <BlogsHero />
+//       <div className="flex gap-10 justify-center py-12 max-w-screen-xl m-auto flex-wrap">
+//         {posts.map((post: BlogCardData) => (
+//           <div key={post.id}>
+//             <BlogsCards {...post} />
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default BlogsPage;
+
+// Import your components and utilities
 import React from 'react';
 import BlogsHero from './BlogsHero';
-
-import { getAllPublished } from '../../lib/notion';
 import BlogsCards from './BlogsCards';
+import { getAllPublished } from '../../lib/notion';
 import { BlogCardData } from '@/shared/types';
-export const revalidate = 10;
-const BlogsPage = async () => {
-  const posts = await getAllPublished();
-  console.log(posts);
+
+// Your page component
+function BlogsPage({ posts }: any) {
   return (
     <>
       <BlogsHero />
@@ -20,6 +45,15 @@ const BlogsPage = async () => {
       </div>
     </>
   );
-};
+}
+
+// Fetch data at build time with ISR
+export async function getStaticProps() {
+  const posts = await getAllPublished();
+  return {
+    props: { posts },
+    revalidate: 10, // Revalidate at most once every 10 seconds
+  };
+}
 
 export default BlogsPage;
